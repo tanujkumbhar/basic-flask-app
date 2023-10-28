818ac1d8831086a5926c4f1d3733999a13722fb1
tanujkumbhar <87263352+tanujkumbhar@users.noreply.github.com>
2023-10-28 23:03:28 +0530

    Changes

--- routes.py
+++ routes.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for, flash
+import MySQLdb
+import MySQLdb.cursors
 
-# two decorators, same function
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'  # Change this to a more secure key
+
+# Connect to MySQL database
+conn = MySQLdb.connect(
+    host='localhost',
+    user='root',
+    passwd='',
+    db='test',
+    unix_socket='/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock',
+    cursorclass=MySQLdb.cursors.DictCursor
+)
+
+
+cursor = conn.cursor()
+
 @app.route('/')
 @app.route('/index.html')
 def index():
-    return render_template('index.html', the_title='Tiger Home Page')
+    return render_template('index.html', the_title='CloudPital Home')
 
 @app.route('/symbol.html')
 def symbol():
@@ -14,6 +30,33 @@ def symbol():
 @app.route('/myth.html')
 def myth():
     return render_template('myth.html', the_title='Tiger in Myth and Legend')
+
+@app.route('/hosp_login.html', methods=['POST', 'GET'])
+def hosp_login():
+    if request.method == 'POST':
+        id = request.form['ID']
+        password = request.form['password']
+
+        cursor = conn.cursor()
+
+        # Check if the hospital with the provided ID and password exists
+        cursor.execute('SELECT * FROM hospital WHERE id=%s AND password=%s', (id, password))
+        hospital = cursor.fetchone()
+
+        cursor.close()
+
+        if hospital:
+            return "Form submitted successfully" 
+            return redirect(url_for('hosp_dashboard'))
+        else:
+            flash('Wrong credentials. Please try again.', 'danger')
+
+    return render_template('hosp_login.html', the_title='Hospital Login')
+
+
+@app.route('/hosp_dashboard.html')
+def hosp_dashboard():
+    return render_template('hosp_dashboard.html', the_title='Hospital Dashboard')
 
 if __name__ == '__main__':
     app.run(debug=True)
